83a2297efe8b05ad4a6f851867f4b225550a2096
medineshkumarm <medineshkumarm@gmail.com>
2024-10-24 22:16:23 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import WebSocketComponent from "./web-socket";
+
+const App = () => {
+  return (
+    <div>
+      <WebSocketComponent />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/web-socket.jsx
@@ -0,0 +1,27 @@
+import useWebSocket from "react-use-websocket";
+
+const WebSocketComponent = () => {
+  const { sendMessage, lastMessage, readyState } = useWebSocket(
+    "ws://localhost:8080"
+  );
+  return (
+    <div className="flex flex-col  justify-center m-2">
+      <div>
+        <button
+          className="bg-emerald-400 p-2 rounded-lg "
+          onClick={() => sendMessage("hello server !")}
+        >
+          {" "}
+          Send Message
+        </button>
+      </div>
+      <div>
+        Last message : {lastMessage ? lastMessage.data : "No messages yet"}
+      </div>
+
+      <div>Connection state: {readyState === 1 ? "Open" : "Closed"}</div>
+    </div>
+  );
+};
+
+export default WebSocketComponent;
